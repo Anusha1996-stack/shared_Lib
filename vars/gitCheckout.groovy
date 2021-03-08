8fa4623815eddb1ad9f0347a367e72db493ed9f7
Anusha1996-stack <79650282+Anusha1996-stack@users.noreply.github.com>
2021-03-07 19:58:59 -0800

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,11 +1,19 @@
 
 
 def call(Map stageParams) {
-	 checkout([
+/*	 checkout([
 		$class: 'GitSCM', 
 		branches: [[name: 
 		stageParams.branch]],extensions: [], 
 		 userRemoteConfigs: [[
 		url: stageParams.url ]]
+	]) 
+*/	
+	checkout([
+		$class: 'GitSCM', 
+		branches: [[name: stageParams.branch]], 
+		extensions: [], userRemoteConfigs: 
+		[[credentialsId: 'f.anusha@globaledgesoft.com', 
+		  url: stageParams.url ]]
 	])
 }
