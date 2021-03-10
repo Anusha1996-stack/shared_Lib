bf7b2bdd06c7ae63db5bea81b81a38200ad7188f
Anusha1996-stack <79650282+Anusha1996-stack@users.noreply.github.com>
2021-03-09 19:55:48 -0800

    Update signintool.py

--- vars/signintool.py
+++ vars/signintool.py
@@ -3,10 +3,11 @@ import stat
 
 #def call (signtype, certificate, filetobesigned, dirforsignedfiles) {
 #    if ( signtype == "openSSL") {
+def pythoncall(one)
 
-st = os.stat('./myfile/f1.py')
-os.chmod('./myfile/f1.py', st.st_mode | stat.S_IEXEC)
-os.system('python ./myfile/f1.py ')
+  st = os.stat('./myfile/f1.py')
+  os.chmod('./myfile/f1.py', st.st_mode | stat.S_IEXEC)
+  os.system('python ./myfile/f1.py ')
 
 #    }
 #}
