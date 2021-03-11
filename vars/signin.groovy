25e3c025f8361b835f20693f1a7deea84a51a01d
Anusha1996-stack <79650282+Anusha1996-stack@users.noreply.github.com>
2021-03-11 17:09:26 +0530

    Update signin.groovy

--- vars/signin.groovy
+++ vars/signin.groovy
@@ -9,10 +9,7 @@ def pythoncall(one):
     os.chmod('./myfile/f1.py', st.st_mode | stat.S_IEXEC)
     os.system('python ./myfile/f1.py ')
 */
-def runMyPython() {
-  final pythonContent = libraryResource('/myfile/f1.py')
-  // There are definitely better ways to do this without having to write to the consumer's workspace
-  writeFile(file: 'f1.py', text: pythonContent)
-  sh('chmod +x f1.py && ./f1.py')
+def python_call() {
+  println "hi"
 }
 
