cb639894ae8175030589c32b0181866d3111ec32
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-03-11 15:34:54 +0530

    modified

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,16 +1,13 @@
 #!/usr/bin/groovy
 
-pipeline
-{
-	stages
-	{
-		stage('checkout')
-		{
-			steps
-			{
-				git branch: "master", url: "https://github.com/Anusha1996-stack/shared_Lib.git"
-			}
-		}
-	}
-}
-				
+node {
+  stage ('checkout')
+  {
+     git branch: "master", url: "https://github.com/Anusha1996-stack/shared_Lib.git" 
+  }                  
+        
+  stage('Running python script')
+  {
+	echo "python call here"
+  }
+}			
