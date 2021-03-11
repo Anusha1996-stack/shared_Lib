b6c2ba68032226b780fd80b75acb6bf5a56508c6
Anusha1996-stack <79650282+Anusha1996-stack@users.noreply.github.com>
2021-03-11 16:56:20 +0530

    Create signintool.py

---
+++ myfile/signintool.py
@@ -0,0 +1,7 @@
+class signintool:
+
+  def python_call():
+    #if (one == 1):
+      st = os.stat('./myfile/f1.py')
+      os.chmod('./myfile/f1.py', st.st_mode | stat.S_IEXEC)
+      os.system('python ./myfile/f1.py ')
