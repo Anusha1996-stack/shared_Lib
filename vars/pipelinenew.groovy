676329f2668a44806404d1e370e918a9c2bc712d
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-03-31 14:47:58 +0530

    modified

--- vars/pipelinenew.groovy
+++ vars/pipelinenew.groovy
@@ -2,12 +2,16 @@
 
 def call() 
 {
+
+	
   node {
+
+	def datas = readYaml file:'Jenkins.yml'
 	stage('git checkout')
 	{
 		println"checkout.......... "
 
-		def datas = readYaml(file: 'Jenkins.yaml')
+		
 		gitclone( gitrepo[i], gitbranch[i],"f52a7452-5c1d-2220-4a97-676f-6bd6-5a6492ae0e36", submodule[i]) //relativedir[i] ) //getAt(i) ) 
 		println"checkout completed.........."	
 	}
