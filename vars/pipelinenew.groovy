62cfa6480e6d2e69dce3197c0facf543887a0b0c
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-04-13 12:25:37 +0530

    updated

--- vars/pipelinenew.groovy
+++ vars/pipelinenew.groovy
@@ -23,28 +23,25 @@ stages
 			println " Reading jenkins.yaml file successfully"
 
 			String git_repo = datas.sourcecode.gitrepo
-    			//String git_branch = datas.sourcecode.gitbranch
-    			//String sub_module = datas.sourcecode.submodule
-			//String relative_dir = datas.sourcecode.checkoutdirectory
-   
     			String[] gitrepo;
-    			//String[] gitbranch;
-    			//String[] submodule;
-			//String[] relativedir;
-   
     			gitrepo = git_repo.split(',') ;
-    			//gitbranch = git_branch.split(',');
-    			//submodule = sub_module.split(',');
-			//relativedir = relative_dir.split(',');
-   
-
-    			for(int i = 0; i < gitrepo.size(); i++)
+    			
+			/*	for(int i = 0; i < gitrepo.size(); i++)
+				{
+					println gitrepo[i] 
+				}*/
+			}
+		}
+	}
+	stage('Compile')
+        {
+                steps
+                {
+			for(int i = 0; i < gitrepo.size(); i++)
 			{
-				println gitrepo[i]
-			//gitclone( gitrepo[i], gitbranch[i], "570d9806-0dc7-4d24-a611-5dc7227f2842", submodule[i]) //relativedir[i] ) //getAt(i) ) 
-			}
-
-			}
+				println gitrepo[i] 
+			}		
+			
 		}
 	}
 }
