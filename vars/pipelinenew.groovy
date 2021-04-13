ff30303f470f9dccb1b2281e1875b79ec46b231f
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-04-13 11:43:06 +0530

    updated

--- vars/pipelinenew.groovy
+++ vars/pipelinenew.groovy
@@ -1,76 +1,28 @@
-/*java.lang.ProcessImpl.waitfor()
-
-def call() 
-{
-	 stash 'jenkins.yaml, vars'
-	
-  node {
-	unstash 'jenkins.yaml, vars'
-	def datas = readYaml file:'Jenkins.yml'
-	stage('git checkout')
-	{
-		println"checkout.......... "
-
-		
-		gitclone( gitrepo[i], gitbranch[i],"f52a7452-5c1d-2220-4a97-676f-6bd6-5a6492ae0e36", submodule[i]) //relativedir[i] ) //getAt(i) ) 
-		println"checkout completed.........."	
-	}
-	}      
-}*/
-
-def call()
+def call(String emaillist)
 {
 def datas
-
 pipeline 
 {
-	agent any
+	agent any {
+	}
+	    
 
 stages
 {
 	stage('Checkout')
 	{
 		steps
-		{       bat ''' dir '''
-		        stash 'jenkins.yaml, vars'
+		{
+			//cleanWs()	
 			script
 			{ 
 
 			datas = readYaml (file: 'jenkins.yaml') 
+			println " Reading jenkins.yaml file successfully"
+
+			
 			}
+			
+			
 		}
 	}
-}
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-node {
-  stage ('checkout')
-  {
-     git branch: "master", url: "https://github.com/Anusha1996-stack/shared_Lib.git" 
-  }                  
-        
-  stage('Running python script')
-  {
-  	  
-	def task = "python myfile/p1.py".execute()
-	task.waitfor()
-	println task.txt
-  }
-}*/
